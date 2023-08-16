64644504e8b3fc57fa88b477c34bddf48ef59ec9
Searchstars <searchstars@163.com>
2023-08-17 00:07:20 +0800

    遮罩

--- Client/Web/src/MainPage.vue
+++ Client/Web/src/MainPage.vue
@@ -8,7 +8,7 @@
         <div class="userinfo_gold_num">13.14</div>
         <img src="./icon/icon_exp.png" class="userinfo_exp_img">
         <div class="userinfo_exp_num">114514</div>
-        <img src="./img/default_profile_img.png" id="avatar">
+        <img src="./img/default_profile_img.png" id="avatar" style="position: absolute;right: 0px;">
       </div>
     </div>
   </body>
